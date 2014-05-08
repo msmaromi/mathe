4ce3fb20ff11d6ed74c932153d255fa6cc13bafa
Muhammad S. Maromi <msmaromi@gmail.com>
2014-05-08 12:42:14 +0700

    update PDA

--- src/mathe/PDA.java
+++ src/mathe/PDA.java
@@ -6,7 +6,11 @@
 
 package mathe;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Vector;
+import sun.reflect.generics.tree.Tree;
 
 /**
  *
@@ -17,12 +21,19 @@ public class PDA {
   public static final String OPERATOR = "operator";
   public static final String OPEN_BRACKET = "(";
   public static final String CLOSE_BRACKET = ")";
+    
+  private ArrayList<Character> listOperator;
+  private ArrayList<Character> listOperan;  
   
   private final String CURRENT_STATE = "current state";
   private final String INPUT = "input";
   private final String TOP_STACK = "top stack";
   private final String NEXT_STATE = "next state";
   private final String RESULT_STACK = "result stack";
+  private final String START_STATE = "start state";
+  private final String FINAL_STATE = "final state";
+  
+  private Vector<Rule> rules;
   
   private class Rule {
     public HashMap<String, String> lefthand;
@@ -37,17 +48,50 @@ public class PDA {
     }
   } 
   
-  public PDA(String currentState, String input, String topStack, String nextState, String resultStack) {
-    
+  public PDA(String filename) {   
+    listOperator = new ArrayList<>(Arrays.asList(new Character[] {'+', '-', '*', '/'}));
+    listOperan = new ArrayList<>(Arrays.asList(new Character[] {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}));
+    rules = extractRules(filename);
+  }
+  
+  public Vector<Rule> extractRules(String filename) {
+    return null;
   }
   
   public Integer analyzeExpression(char[] listChar) {
+    String state = START_STATE;
+    outerloop:
+    for(int i = 0; i < listChar.length; i++) {
+      String typeInput = examineExpresseion(listChar[i]);      
+      if(typeInput == null)
+        return null;
+      else {
+        for(int j = 0; j < rules.size(); j++) {
+          Rule rule = rules.get(j);
+          if(rule.lefthand.get(CURRENT_STATE) == state && rule.lefthand.get(INPUT) == typeInput) {
+            state = rule.lefthand.get(CURRENT_STATE);
+            break outerloop;
+          }          
+        }
+      }
+    }
     
+//    if(state == FINAL_STATE)
+      
+      
     return null;
   }
   
-  public String examineExpresseion(char c) {
+  public String examineExpresseion(char c) {    
+    if(listOperator.contains(c))
+      return OPERATOR;
+    else if(listOperan.contains(c))
+      return OPERAN;
+    else if(c == '(')
+      return OPEN_BRACKET;
+    else if(c == ')')
+      return CLOSE_BRACKET;
     
     return null;
-  }
+  }    
 }
